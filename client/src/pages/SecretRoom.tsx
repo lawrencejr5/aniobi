d328f2301184150455a2b255d852bcceccdc60a3
lawrencejr <oputalawrence@gmail.com>
2025-02-23 21:08:48 +0100

    secret room js functions for moving forward and backward

--- client/src/pages/SecretRoom.tsx
+++ client/src/pages/SecretRoom.tsx
@@ -4,80 +4,64 @@ import Nav from "../components/Nav";
 
 import { useGlobalContext } from "../Global";
 
-import {
-  FaPaperPlane,
-  FaTimes,
-  FaSpinner,
-  FaChevronLeft,
-  FaChevronRight,
-} from "react-icons/fa";
-import { FaArrowUpLong, FaRotate } from "react-icons/fa6";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaRotate } from "react-icons/fa6";
 
 const SecretRoom = () => {
-  const [commentOpen, setCommentOpen] = React.useState<boolean>(false);
-  const [focused, setFocused] = React.useState<Boolean>(false);
-  const [input, setInput] = React.useState<string>("");
+  const { messages, getMessages }: any = useGlobalContext();
 
-  const openComments = () => {
-    setCommentOpen((prev) => {
-      return !prev;
-    });
+  const [curr, setCurr] = React.useState<number>(0);
+
+  const move = (position: string) => {
+    if (position === "forward") {
+      setCurr((prev) => {
+        if (prev == messages?.length - 1) return 0;
+        return prev + 1;
+      });
+    } else if (position === "backward") {
+      setCurr((prev) => {
+        if (prev == 0) return messages?.length - 1;
+        return prev - 1;
+      });
+    }
   };
 
-  const { messages }: any = useGlobalContext();
+  const reload = (): void => {
+    getMessages();
+    setCurr(0);
+  };
+
+  React.useEffect(() => {
+    getMessages();
+  }, []);
 
   return (
     <div className="secret-room-container">
       <Nav page={"secret"} />
-
-      <div className="reload">
+      <div className="reload" onClick={reload}>
         <FaRotate />
       </div>
-      <div className="item-container">
-        <div className="move left">
-          <FaChevronLeft />
-        </div>
-        {/* {messages.map(({ message }: { message: string }) => {
-          return (
-            <div className="item">
-              <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
-              <div className="msg">{message}</div>
-            </div>
-          );
-        })} */}
-        <div className="items">
-          <div className="item">
-            <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
-            <div className="msg">
-              I don’t believe in spirits of any kind that includes gods I don’t
-              believe in spirits of any kind that includes gods
-            </div>
+      <div className="body">
+        <h1>Shhh!...🤫, u've just found secret room🧐</h1>
+        <div className="item-container">
+          <div className="move left" onClick={() => move("backward")}>
+            <FaChevronLeft />
           </div>
-          <div className="item">
-            <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
-            <div className="msg">
-              I don’t believe in spirits of any kind that includes gods I don’t
-              believe in spirits of any kind that includes gods
-            </div>
+          <div className="items">
+            {messages.map(({ message }: { message: string }, i: number) => {
+              return (
+                <div className={`item ${curr == i && "active"}`} key={i}>
+                  <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
+                  <div className="msg">{message}</div>
+                </div>
+              );
+            })}
           </div>
-          <div className="item active">
-            <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
-            <div className="msg">
-              I don’t believe in spirits of any kind that includes gods I don’t
-              believe in spirits of any kind that includes gods
-            </div>
-          </div>
-          <div className="item">
-            <img src="/imgs/aniobi_transparent_oxblood.png" alt="" />
-            <div className="msg">
-              I don’t believe in spirits of any kind that includes gods I don’t
-              believe in spirits of any kind that includes gods
-            </div>
-          </div>
-        </div>
 
-        <div className="move left">
-          <FaChevronRight />
+          <div className="move left" onClick={() => move("forward")}>
+            &nbsp;
+            <FaChevronRight />
+          </div>
         </div>
       </div>
     </div>
