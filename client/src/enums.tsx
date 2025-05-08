0980b48cf9bef1e719229016d2a18308edf49250
lawrencejr5 <oputalawrence@gmail.com>
2025-05-08 18:00:49 +0100

    liking server code

--- client/src/enums.tsx
+++ client/src/enums.tsx
@@ -1,9 +1,10 @@
 export enum EndPoints {
   messages = "http://localhost:5000/api/v1/messages",
   admin = "http://localhost:5000/api/v1/admin",
-  comment = "http://localhost:5000/api/v1/comment"
+  comment = "http://localhost:5000/api/v1/comment",
   // messages = "https://aniobi-api.vercel.app/api/v1/messages",
   // admin = "https://aniobi-api.vercel.app/api/v1/admin",
+  // comment = "https://aniobi-api.vercel.app/api/v1/comment",
 }
 
 export const LocalStorage = {
